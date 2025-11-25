53804181d5b1b5989d2be3aa947a85fa57810e73
diymaker3d <maxibalsamico@gmail.com>
2025-11-25 12:03:25 +0100

    Add files via upload

---
+++ websitebongo.py
@@ -0,0 +1,26 @@
+from urllib import request
+print("")
+website = ()
+command = ()
+x = ()
+def help():
+    print ("this is websitebongo")
+    print ("the following commands are used as listed")
+    print ("dtf (this command stand for downloading files of the giging websites to an generated file)")
+    print ("help (the standart command for seeing what commands are possible to use)")
+    exit
+def dtf ():#downloadtofile
+    exit
+  
+while True :               #main loop wich is just asking fo an comman and can be exitted vie ctrl c
+    command = input()
+    if command == "--help" :
+        help()
+    elif command == "dtf":
+        website = input()
+        x = request.urlopen(website)
+        print(type(x))
+        print(dir(x))
+    else:
+        help()
+
