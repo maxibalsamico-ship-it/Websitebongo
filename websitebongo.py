e52bc2636e852ac0043f064f04e8af85c6431663
diymaker3d <maxibalsamico@gmail.com>
2025-12-02 11:35:09 +0000

    kp

--- websitebongo.py
+++ websitebongo.py
@@ -1,26 +1,40 @@
 from urllib import request
 import pickle
+import os
 print("")
 website = ()
 command = ()
 x = ()
-def help():
+filename = ()
+leer = ()
+def help ():
     print ("this is websitebongo")
     print ("the following commands are used as listed")
     print ("dtf (this command stand for downloading files of the giging websites to an generated file)")
     print ("help (the standart command for seeing what commands are possible to use)")
     exit
-def dtf ():#downloadtofile
-    website = input()
-    print (website)
-    x = request.urlopen(website)
-    print(type(x))
-    print(dir(x))
-    data = x.read()  
-    with open("dumpfile.txt", "wb") as dumpfile:
-    pickle.dump(data, dumpfile)
-    exit
-  
+def dtf (filename):#downloadtofile
+    if filename != leer
+         website = input()
+        print (website)
+        x = request.urlopen(website)
+        print(type(x))
+        print(dir(x))
+        data = x.read()
+        os.mkdir(filename)
+        with open(filename, "wb") as filename:
+        pickle.dump(data, filename)
+        exit
+    else
+        website = input()
+        print (website)
+        x = request.urlopen(website)
+        print(type(x))
+        print(dir(x))
+        data = x.read()  
+        with open("dumpfile.txt", "wb") as dumpfile:
+        pickle.dump(data, dumpfile)
+        exit
 while True :               #main loop wich is just asking fo an comman and can be exitted vie ctrl c
     command = input()
     if command == "--help" :
